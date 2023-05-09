390ac7c6e6271c508aea06a7588ded90c1e1307e
Benjamin Pham <benjaminpham@Benjamins-MacBook-Air.local>
2023-05-09 16:50:36 -0700

    Change font units and add image hover effect for latest.html

--- f1-website/scripts/latest.js
+++ f1-website/scripts/latest.js
@@ -5,7 +5,29 @@ newsContentText.forEach(element => {
         element.style.borderColor = '#e10600';
     });
 
+    // element.parentElement.addEventListener('mouseenter', () => {
+    //     element.previousElementSibling.style.transform = "scale(1.1)";
+    // });
+
     element.parentElement.addEventListener('mouseleave', () => {
         element.style.borderColor = '';
+    });
+
+    // element.parentElement.addEventListener('mouseleave', () => {
+    //     element.previousElementSibling.style.transform = "";
+    // });
+});
+
+const exploreButtons = document.querySelectorAll('.explore a');
+
+exploreButtons.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        element.firstElementChild.style.filter = "brightness(40%)";
+        element.firstElementChild.style.transform = "scale(1.1)";
+    });
+
+    element.addEventListener('mouseleave', () => {
+        element.firstElementChild.style.filter = "";
+        element.firstElementChild.style.transform = "";
     });
 });
